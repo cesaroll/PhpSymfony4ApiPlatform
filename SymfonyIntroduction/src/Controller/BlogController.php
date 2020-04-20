17b631da2e041153883c61de9d8ff22e6e0e86c1
Cesar Lopez <clopezlerma@wayfair.com>
2020-04-20 23:04:20 +0200

    Abstract Controller, Request, Response

--- SymfonyIntroduction/src/Controller/BlogController.php
+++ SymfonyIntroduction/src/Controller/BlogController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -32,10 +33,14 @@ class BlogController extends AbstractController {
   /**
    * @Route("/{page}", name="blog_list", defaults={"page"=5})
    */
-  public function list($page = 1) {
-    return new JsonResponse(
+  public function list($page = 1, Request $request) {
+
+    $limit = $request->get('limit', 10);
+
+    return $this->json(
       [
         'page' => $page,
+        'limit' => $limit,
         'data' => array_map(function ($item) {
           return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
         }, self::POSTS)
@@ -47,7 +52,7 @@ class BlogController extends AbstractController {
    * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
    */
   public function post(int $id) {
-    return new JsonResponse(
+    return $this->json(
       self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
     );
   }
@@ -57,7 +62,7 @@ class BlogController extends AbstractController {
    */
   public function postBySlug(string $slug)
   {
-    return new JsonResponse(
+    return $this->json(
       self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
     );
   }
