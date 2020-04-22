292511c2405384d9e4b55ef9e0a6794421da2f9e
Cesar Lopez <clopezlerma@wayfair.com>
2020-04-22 23:06:18 +0200

    Migration - modifying database structure

--- SymfonyIntroduction/src/Entity/BlogPost.php
+++ SymfonyIntroduction/src/Entity/BlogPost.php
@@ -36,6 +36,12 @@ class BlogPost
      */
     private $author;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     */
+    private $slug;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +92,17 @@ class BlogPost
     {
         $this->author = $author;
 
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
         return $this;
     }
 }
