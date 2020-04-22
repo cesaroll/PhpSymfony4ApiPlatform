a1dfc6587fb8186d093e65a020d12e08349bf2e2
Cesar Lopez <clopezlerma@wayfair.com>
2020-04-22 23:41:17 +0200

    Serilizing data and persisting entities

--- SymfonyIntroduction/src/Controller/BlogController.php
+++ SymfonyIntroduction/src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,7 +31,7 @@ class BlogController extends AbstractController {
   ];
 
   /**
-   * @Route("/{page}", name="blog_list", defaults={"page"=5})
+   * @Route("/{page}", name="blog_list", defaults={"page"=5}, requirements={"page"="\d+"})
    */
   public function list($page = 1, Request $request) {
 
@@ -59,11 +60,28 @@ class BlogController extends AbstractController {
   /**
    * @Route("/{slug}", name="blog_by_slug")
    */
-  public function postBySlug(string $slug)
+  /*public function postBySlug(string $slug)
   {
     return $this->json(
       self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
     );
+  }*/
+
+  /**
+   * @route("/add", name="blog_add", methods={"POST"})
+   */
+  public function add(Request $request) {
+
+    /** @var Serializer $serializer */
+    $serializer = $this->get('serializer');
+
+    $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+    $em = $this-> getDoctrine()->getManager();
+    $em->persist($blogPost);
+    $em->flush();
+
+    return $this->json($blogPost);
   }
 
 }
